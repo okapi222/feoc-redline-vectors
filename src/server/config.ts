0f11a8603a673f10f1b7f80afd0eb2866b7157eb
okapi222 <herskowitz.daniel@gmail.com>
2026-01-13 11:10:46 -0500

    Create config.ts

---
+++ src/server/config.ts
@@ -0,0 +1,5 @@
+export const MANIFEST_URL =
+  "https://raw.githubusercontent.com/okapi222/feoc-redline-vectors/refs/heads/main/v1.0.1/manifest.v1.0.1.json";
+
+export const RUNTIME_INSTRUCTIONS_URL =
+  "https://raw.githubusercontent.com/okapi222/feoc-redline-vectors/refs/heads/main/v1.0.1/runtime_instructions.v0.json";
